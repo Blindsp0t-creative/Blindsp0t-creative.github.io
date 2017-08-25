9f37e0f5587498e267255fdefbe9c4d702ea62d3
Blindsp0t <fokdemall@gmail.com>
2017-08-25 16:11:45 +0200

    aspect ratio

--- GlitchySelfie/sketch.js
+++ GlitchySelfie/sketch.js
@@ -68,30 +68,14 @@ function draw() {
             img.updatePixels();
         }
         //image(img, 0, 0, w, h);
-        if(windowHeight > windowWidth)
-            {
-                image(img, 0,0,windowHeigt/webcamAspectRatio,windowHeight); 
-            }
-        else
-            {
-                image(img, 0,0,windowWidth,windowWidth/webcamAspectRatio);        
-            }
-        
+        image(img, 0,0,windowWidth,windowWidth/webcamAspectRatio);
     }
 
     if(live==1)
     {
         capture.loadPixels();
         //image(capture, 0, 0, w, h);
-        if(windowHeight > windowWidth)
-            {
-                image(capture, 0,0,windowHeigt/webcamAspectRatio,windowHeight);
-            }
-        else
-            {
-                image(capture, 0,0,windowWidth,windowWidth/webcamAspectRatio);        
-            }
-        
+        image(capture, 0,0,windowWidth,windowWidth/webcamAspectRatio);
     }
 }
 
